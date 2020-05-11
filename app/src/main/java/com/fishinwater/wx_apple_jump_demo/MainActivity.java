18d315fa97b771e693c533e8ada2a8d0b1beeb8c
FishInWater-1999 <814484626@qq.com>
2020-05-11 17:12:31 +0800

    Initial commit

---
+++ app/src/main/java/com/fishinwater/wx_apple_jump_demo/MainActivity.java
@@ -0,0 +1,18 @@
+package com.fishinwater.wx_apple_jump_demo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+
+
+}
